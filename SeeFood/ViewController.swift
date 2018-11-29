2653a1b2dd0cfa933f11ad6fca39a704f0f56561
Lucas Dahl <LucasDahl425@yahoo.com>
2018-11-29 13:34:26 -0800

    Set up the present for the camera VC.

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -15,14 +15,31 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     // Outlets
     @IBOutlet weak var imageView: UIImageView!
     
+    // Properties
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Set the delegate for the imagePicker
+        imagePicker.delegate = self
+        
+        // Set allows type for the camera
+        imagePicker.sourceType = .camera
+        
+        // Set if the user can edit the image
+        imagePicker.allowsEditing = false
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        <#code#>
     }
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
         
-        
+        // Present the camera VC(image picker or camera)
+        present(imagePicker, animated: true, completion: nil)
         
     }
     
