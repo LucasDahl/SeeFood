4eb3dd0450d8650fca44e77c35d56d0af9464155
Lucas Dahl <LucasDahl425@yahoo.com>
2018-11-29 13:26:22 -0800

    Setup the UI and hooked up Ib outlets/actions.

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -10,13 +10,21 @@ import UIKit
 import CoreML
 import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // Outlets
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
-
-}
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        
+        
+    }
+    
+} // End class
 
