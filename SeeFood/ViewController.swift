dffac3de0ae57dea05ee6ba02ee7838ff4dfa0e3
Lucas Dahl <LucasDahl425@yahoo.com>
2018-11-30 07:24:40 -0800

    Added a method that uses the Model to classify the image taken or chosen by the user. Currently only prints to the console.

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -40,10 +40,47 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             // Set the imageView
             imageView.image = userPickedImage
             
+            // Convert the UIImage to the CIImage
+            guard let ciimage = CIImage(image: userPickedImage) else { fatalError("Could not convert to CIImage") }
+            
+            // Detect the image
+            detect(image: ciimage)
+            
         }
         
         // Dismiss the imagepicker
         imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func detect(image: CIImage) {
+        
+        // This is a container for the model InceptionV3
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else { fatalError("Error loading CoreML Model") }
+        
+        // Create a vision core request
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            
+            // Get the results
+            guard let results = request.results as? [VNClassificationObservation] else { fatalError("Couldn't get the results because the MOdel failed to proces images") }
+            
+            print(results)
+            
+        }
+        
+        // Get the handler for the request to specify the image want to classify
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            
+            try handler.perform([request])
+            
+        } catch {
+            
+            // Print handler error
+            print("Handler error: \(error)")
+            
+        }
         
     }
 
